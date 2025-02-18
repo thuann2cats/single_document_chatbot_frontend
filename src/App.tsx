1dea53ff13f5ee0d49015c61b3f3b43921bc18f0
Thuan Nguyen <nguyenthuan49@gmail.com>
2025-02-17 20:21:46 -0500

    adding bottom message

--- src/App.tsx
+++ src/App.tsx
@@ -152,6 +152,9 @@ function App() {
             />
             <button type="submit">Send</button>
           </form>
+          <div className="feedback-message">
+          This simple user interface is meant for testing an LLM's ability to answer users' questions on an uploaded document. Chat history is automatically saved. Please send any feedback to thuan.nguyen208@gatech.edu and copy the URL.
+          </div>
         </>
       )}
     </div>
